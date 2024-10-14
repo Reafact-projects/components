7f7fbf7f7f4902fca678fe208731103a7e8c5d97
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-10-15 00:32:31 +0100

    get ride of unused code

--- EatzzZ-React-master/src/components/Feature/FeatureElements.js
+++ EatzzZ-React-master/src/components/Feature/FeatureElements.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
-import FeaturePic from '../../images/chickentandoori.jpg';
 
 export const FeatureContainer = styled.div`
   background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
-    url(${FeaturePic});
   height: 100vh;
   max-height: 500px;
   background-position: center;
@@ -23,20 +21,5 @@ export const FeatureContainer = styled.div`
   p {
     margin-bottom: 1rem;
     font-size: clamp(1rem, 3vw, 2rem);
-  }
-`;
-export const FeatureButton = styled.button`
-  font-size: 1.4rem;
-  padding: 0.6rem 3rem;
-  border: none;
-  background: #ffc500;
-  color: #000;
-  transition: 0.2s ease-out;
-
-  &:hover {
-    color: #fff;
-    background: #e31837;
-    transition: 0.2s ease-out;
-    cursor: pointer;
   }
 `;
